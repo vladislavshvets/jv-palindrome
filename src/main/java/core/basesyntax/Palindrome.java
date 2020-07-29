01b31294560cc070685d4f455214f26fa546b8a9
Vladislav <48126402+vladislavshvets@users.noreply.github.com>
2020-07-29 21:44:12 +0300

    added solution

--- src/main/java/core/basesyntax/Palindrome.java
+++ src/main/java/core/basesyntax/Palindrome.java
@@ -21,6 +21,7 @@ public class Palindrome {
      * <p>Результат: true</p>
      */
     public boolean isPalindrome(String text) {
-        return false;
+        String withoutSymbols = text.replaceAll("[^A-Za-zА-Яа-я0-9]", "").toLowerCase();
+        return withoutSymbols.equals(new StringBuilder(withoutSymbols).reverse().toString());
     }
 }
